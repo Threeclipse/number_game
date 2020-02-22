2105237a116c7a21750f0650fbea00c3c1b9a923
Burntwilly <burntwilly@Burntwillys-MacBook-Pro.local>
2020-02-21 17:37:40 -0800

    changed low to minimum changed high to maximum

--- number_game.py
+++ number_game.py
@@ -1,19 +1,19 @@
 from random import randint
 
 # Minimum number in range
-low = 0
+minimum = 0
 
 # Maximum number in range
-high = 30
+maximum = 30
 
-# Number of allowed player-input guesses
+# Number of almined player-input guesses
 guesses = 3
 
 # Initiate a random number that will be the basis of the game
-random_num = randint(low, high)
+random_num = randint(minimum, maximum)
 
 # The game chooses the first number for the player
-first_num = randint(low, high)
+first_num = randint(minimum, maximum)
 
 # Input validation
 def inputNumber(message):
@@ -34,7 +34,7 @@ while i < guesses:
 	i = i + 1
 	# Automatic first guess made by system
 	if i == 1:
-		print(f"\nI'm thinking of a number between {low} and {high}.\nYou have {guesses} tries to find the number.\nTo make this more interesting, I'm going to make your first guess for you.\n\n...and that number is {first_num}.")
+		print(f"\nI'm thinking of a number between {minimum} and {maximum}.\nYou have {guesses} tries to find the number.\nTo make this more interesting, I'm going to make your first guess for you.\n\n...and that number is {first_num}.")
 		if first_num < random_num:
 			print(f"\n{first_num} is too low!\n")
 		if first_num > random_num:
